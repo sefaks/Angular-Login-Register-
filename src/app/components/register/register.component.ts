2810e6c050cf9e5c30e55811bf0ce50b3d7e643c
Yusuf Sefa <68388936+sefaks@users.noreply.github.com>
2024-03-07 22:45:31 +0300

    registerdone

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+
+  registerForm = this.fb.group({
+    fullName:['',[Validators.required,Validators.pattern(/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/)]],
+    email:['', [Validators.required,Validators.email]],
+     password:['', Validators.required],
+     confirmPassword:['',Validators.required]
+
+
+  })
+  constructor(private fb:FormBuilder){}
+
+
+  get fullName(){
+    return this.registerForm.controls['fullName'];
+  }
+  get email(){
+
+    return this.registerForm.controls['email'];
+  }
+  get password(){
+
+    return this.registerForm.controls['password'];
+  }
+
+  get confirmPassword(){
+    return this.registerForm.controls['confirmPassword'];
+  }
 
 }

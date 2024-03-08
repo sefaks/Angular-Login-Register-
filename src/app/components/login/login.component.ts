0a609c60ee3207624139f1664dd7fa7a71d4ed90
Yusuf Sefa <68388936+sefaks@users.noreply.github.com>
2024-03-08 10:03:15 +0300

    errors

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-login',
@@ -15,7 +18,7 @@ export class LoginComponent {
 
       })
 
-        constructor(private fb:FormBuilder){}
+        constructor(private fb:FormBuilder,private authService:AuthService,private router:Router,private messageService:MessageService){}
 
         get email(){
 
@@ -24,5 +27,24 @@ export class LoginComponent {
         get password(){
 
           return this.loginForm.controls['password'];
+        }
+
+        loginUser(){
+          const{email,password} = this.loginForm.value;
+          this.authService.getUserByEmail(email as string).subscribe(
+            response=>{
+              if(response.length>0 && response[0].password == password){
+                sessionStorage.setItem('email',email as string);
+                this.router.navigate(['/home']);
+              }else{
+                this.messageService.add({severity: 'error', summary:'Error',detail:'email or password is wrong'});
+              }
+            },
+            error =>
+            {
+              this.messageService.add({severity: 'error', summary:'Error',detail:'Something went wrong'});
+
+            }
+          )
         }
 } 
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,17 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = http://localhost:3000;
+  baseUrl = 'http://localhost:3000';
 
 
   constructor(private http:HttpClient) { }
 
-  registerUser(userDetails:User){
-    return this.http.post(`${this.baseUrl}/users`,userDetails)
+  registerUser(userDetails: User) {
+    return this.http.post(`${this.baseUrl}/users`, userDetails)
   }
+
+  getUserByEmail(email:string) :Observable<User[]>{
+  return this.http.get<User []>(`${this.baseUrl}/users?email=${email}`);
+
+}
 }
